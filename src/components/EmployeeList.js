944f3ffad8e57916d02152254dbdbb3a717e08b0
doniarobinson <doniamae@gmail.com>
2018-12-02 19:21:05 -0600

    Add React table

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
 import Employee from './Employee';
+
+import './EmployeeList.css';
 
 var flattenObject = require('flatten-object');
 
@@ -23,11 +28,36 @@ class EmployeeList extends React.Component {
     }
 
     render() {
+        const staticEmployees  = [
+            { first_0: "emp1", last_0: "last1" },
+            { first_0: "emp2", last_0: "last2" }
+        ];
+        console.log(typeof staticEmployees);
         return (
-          <div className="EmployeeList">
+            <div className="EmployeeList">
     
             <h4>Employee List</h4>
-            <Employee {...this.state.flatEmployees} />
+
+            { this.state && this.state.flatEmployees &&
+                <div>
+                <ReactTable
+                data={staticEmployees}
+                columns={[
+                    {
+                        Header: "First Name",
+                        accessor: "first_0"
+                    },
+                    {
+                        Header: "Last Name",
+                        id: "last_0",
+                        accessor: d => d.last_0
+                    }
+                ]}
+                defaultPageSize={10}
+                className="-striped -highlight"
+                />
+                </div>
+            }
     
           </div>
         );
